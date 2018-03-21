a1607c5a8d0dd14a6050244ac6f21b5bcb8c3fa1
zhangxy <491419075@qq.com>
2018-03-21 16:37:15 +0800

    修改6-1
    正则表达式不完整 有点问题

--- Chapter6/6-1-JDPhonePicCrawler.py
+++ Chapter6/6-1-JDPhonePicCrawler.py
@@ -1,26 +1,39 @@
+#coding:utf-8
 import re
 import urllib.request
 import urllib.error
 
 def crawJD(url,page):
-    html1 = urllib.request.urlopen(url).read()
+
+    html1 = urllib.request.urlopen(url).read().decode("utf-8")
+
     html1 = str(html1)
 
+
     pat1 = '<div id="plist".+? <div class="page clearfix">'
-    result1 = re.compile(pat1).findall(html1)
+    result1 = re.compile(pat1,re.S).findall(html1)
     result1 = result1[0]
 
-    pat2 = '<img width="220" height="220" data-img="1" src="//(.+?\.jpg)">'
+    pat2 = '<img width="220" height="220" data-img="1" data-lazy-img="//(.+?\.jpg)">'
     imagelist = re.compile(pat2).findall(result1)
 
-    # 添加手机名称
-    namepat  = "<em>(.*?)</em>"
-    namelist = re.compile(namepat,re.S).findall(result1)
-    print(unicode(str(namelist[3])),"utf-8")
+    # #添加手机名称 正则不完整
+    # namepat  = '<em>(.*?)</em>'
+    # namelist = re.compile(namepat,re.S).findall(result1)
+    # nlist=[]
+    # for name in namelist:
+    #     if name!="":
+    #         aname = name.replace("\n","").replace("/","").strip()
+    #         nlist.append(aname)
+    #
+    #
+    # print(len(nlist))
+
 
     index = 1
     for imageurl in imagelist:
-        imagename = "D:/zxy/JDpic/"+str(page)+str(index)+".jpg"
+        #imagename="D:/zxy/JDpic/"+str(page)+str(index)+nlist[index-1]+".jpg"
+        imagename = "D:/zxy/JDpic/" + str(page) + str(index)+ ".jpg"
         imageurl = "http://"+imageurl
         try:
             urllib.request.urlretrieve(imageurl,filename=imagename)
